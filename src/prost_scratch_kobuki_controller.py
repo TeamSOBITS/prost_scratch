7ecdcc44c90d6e1c4f68718f1b133d893066a1c3
yanobtz <e18m5215@soka-u.jp>
2019-05-27 05:37:09 +0900

    ラズパイ側自動git pull確認用コメント--その5

--- src/prost_scratch_kobuki_controller.py
+++ src/prost_scratch_kobuki_controller.py
@@ -369,7 +369,7 @@ class OdomBaseController:
 						end.data = "move end"
 						self.pub_retrun_arrive.publish(end)
 		rospy.loginfo("Moving Finished")
-		#ラズパイ側自動git pull確認用コメント--その4
+		#ラズパイ側自動git pull確認用コメント--その5
 		#rospy.loginfo("---- %s" % motion.data)
 
 
