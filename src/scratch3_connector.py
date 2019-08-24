974de78038e0564d83f128bb56c3c972faac86c0
yanobtz <e18m5215@soka-u.jp>
2019-08-24 23:24:00 +0900

    立ち上げタイミングのずらしを元に戻した

--- src/scratch3_connector.py
+++ src/scratch3_connector.py
@@ -8,7 +8,6 @@ import PIL.Image
 import numpy as np
 import tf
 import math
-from subprocess import Popen
 from std_msgs.msg import String,UInt8,Empty,Bool
 from geometry_msgs.msg import Twist,Quaternion,PoseStamped
 from sensor_msgs.msg import LaserScan,Image
@@ -57,10 +56,6 @@ class Scratch3Connector:
 		self.save_qr_distance = 0
 		self.save_qr_width = 0
 		self.save_qr_angle = 0
-
-		rospy.sleep(3)
-		#QR認識
-		qr_node = Popen( ["roslaunch","prost_scratch","minimal.launch"] )
 
 	def cb_wifi_connect(self, state):
 		if state.data == True:
