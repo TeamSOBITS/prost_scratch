330be19765e2e5416256d8c39fd9b9d9fd5c8a5b
yanobtz <e18m5215@soka-u.jp>
2019-09-28 01:24:18 +0900

    移動時の速度の上下限を設定&変更

--- src/scratch3_connector.py
+++ src/scratch3_connector.py
@@ -48,6 +48,8 @@ class Scratch3Connector:
 		self.save_qr_distance = 0
 		self.save_qr_width = 0
 		self.save_qr_angle = 0
+		self.lower_limit_speed = -45
+		self.upper_limit_speed = 45
 
 		self.sub_qr_position = rospy.Subscriber("/visp_auto_tracker/object_position", PoseStamped, self.qr_position)
 
@@ -80,8 +82,10 @@ class Scratch3Connector:
 			self.pub_odom_base_ctrl.publish(self.get_msg)
 		elif(self.get_msg.find('move_speed:') >= 0):
 			word = self.get_msg[11:len(self.get_msg)]
-			if float(word) > 30:
-				word = "30"
+			if float(word) > self.upper_limit_speed:
+				word = "45"
+			elif float(word) < self.lower_limit_speed:
+				word = "-45"
 			self.moving_speed.linear.x = float(word) * 0.01
 			self.moving_speed.angular.z = 0.0
 			self.pub_twist.publish(self.moving_speed)
