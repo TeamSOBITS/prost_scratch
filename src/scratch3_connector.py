97206a45245928d88cb625b92daf0c88d56c946d
yanobtz <github365@gmail.com>
2020-09-02 16:00:52 +0900

    second commit

--- src/scratch3_connector.py
+++ src/scratch3_connector.py
@@ -49,7 +49,7 @@ class Scratch3Connector:
 		time.sleep(3)
 		connection_call = String()
 		connection_call = "USBを接続した後に、接続ブロックを実行してください"
-		#self.pub_speech_word.publish(connection_call)
+		self.pub_speech_word.publish(connection_call)
 
 
 	def cb_wifi_connect(self, state):
