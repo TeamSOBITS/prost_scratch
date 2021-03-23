8d8fba8950cc4f56398f08f857b43786688a395b
project_studies <choi.7f@gmail.com>
2021-03-23 04:16:51 +0000

    Update wifi_connect_sim.py

--- src/wifi_connect_sim.py
+++ src/wifi_connect_sim.py
@@ -1,1 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Search for a specific wifi ssid and connect to it.
+written by kasramvd.
+"""
+import os
+from subprocess import check_output,Popen
+import rospy
+from std_msgs.msg import Bool
 
+if __name__ == "__main__":
+    rospy.init_node('wifi_connect')
+    rospy.loginfo("wifi connect check Started")
+    IP = "1"#接続を確認したいIP設定
+    #IP = "172.29.129."#テスト用 (SOKAWiFi)
+    #IP = "192.168.11."#テスト用 (F710_5G)
+    #IP = "192.168.0."#テスト用（家）
+    connect_state = Bool()
+    node_kill_flag = False
+    pub_wifi_connect = rospy.Publisher('/wifi_connect', Bool, queue_size=10)
+
+    while not rospy.is_shutdown():
+        wifi_ip = check_output(['hostname', '-I'])
+        #print(wifi_ip)
+        if IP in wifi_ip:
+            if node_kill_flag == True:
+            	node_run = Popen(["roslaunch","rosbridge_server","rosbridge_websocket.launch"])
+            	rospy.sleep(3)
+            	node_kill_flag = False
+            #print('ok')
+            connect_state.data = True
+            pub_wifi_connect.publish(connect_state)
+            rospy.sleep(1)
+        else:
+            print('no')
+            connect_state.data = False
+            pub_wifi_connect.publish(connect_state)
+            node_kill_flag = True
+            node_kill = Popen(["rosnode","kill","/rosbridge_websocket"])
+            rospy.sleep(3)
