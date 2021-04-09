bc351952042a2825f8d76ac639e295ee60e3dbed
agurakakenai <you@example.com>
2021-04-09 19:19:56 +0900

    2021年度春学期用に修正

--- src/wifi_connect_sim.py
+++ src/wifi_connect_sim.py
@@ -12,7 +12,11 @@ from std_msgs.msg import Bool
 if __name__ == "__main__":
     rospy.init_node('wifi_connect')
     rospy.loginfo("wifi connect check Started")
-    IP = "192.168.1."#接続を確認したいIP設定
+    #IP = "192.168.1."#接続を確認したいIP設定
+    #IP = "192.168.11."#テスト用 (F710_5G)
+    #IP = "172.23.139."#テスト用 (SOKA30WL)
+    #IP = "192.168.0."#接続を確認したいIP設定(家)
+    IP = "1"#シミュレーション用万能設定
     connect_state = Bool()
     node_kill_flag = False
     pub_wifi_connect = rospy.Publisher('/wifi_connect', Bool, queue_size=10)
